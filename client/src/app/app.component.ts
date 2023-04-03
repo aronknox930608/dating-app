207e2385c30bb95cde9fd145b49d54c5260b59d2
aronknox0608 <aronknox0608@gmail.com>
2023-04-03 13:49:29 -0300

    :heavy_plus_sign: build: install angular, bootstrap and font-awesome

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/users").subscribe({
+      next: (response) => this.users = response,
+      error: (error) => console.log(error),
+      complete: () => console.log("Complete"),
+    })
+  }
+
+}
